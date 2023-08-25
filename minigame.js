0efbcd483546bd469717488a3dc7ddacab500fac
Dmitry Silin <silinlicious@gmail.com>
2023-08-25 20:08:29 +0400

    Update minigame.js

--- minigame.js
+++ minigame.js
@@ -1,8 +1,6 @@
-// Получаем элементы из DOM
 const block1 = document.getElementById('block1');
 const block2 = document.getElementById('block2');
 
-// Функция для генерации случайных координат
 function getRandomPosition() {
   const maxWidth = window.innerWidth - 200;
   const maxHeight = window.innerHeight - 200;
@@ -11,7 +9,7 @@ function getRandomPosition() {
   return { x: randomX, y: randomY };
 }
 
-// Функция для обновления положения блоков
+
 function updateBlocks() {
   const position1 = getRandomPosition();
   const position2 = getRandomPosition();
@@ -23,15 +21,15 @@ function updateBlocks() {
   block2.style.top = position2.y + 'px';
 }
 
-// Обработчик события клика на блоках
+
 function handleClick(event) {
   event.target.style.backgroundColor = 'blue';
 }
 
-// Добавляем обработчик события клика на блоках
+
 block1.addEventListener('click', handleClick);
 block2.addEventListener('click', handleClick);
 
-// Обновляем положение блоков при загрузке страницы и каждые 0.5 секунды
+
 window.addEventListener('load', updateBlocks);
 setInterval(updateBlocks, 500);
